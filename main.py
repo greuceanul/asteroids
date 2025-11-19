4e7adcd3552b8fb9e4a4553f01d9e71356cc1306
darth-kratos <catalin.tucra@gmail.com>
2025-11-19 21:41:49 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
