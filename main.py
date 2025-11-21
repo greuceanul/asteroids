21eb36d5ac267bfd6487eb76f4e8dbe56ae95297
darth-kratos <catalin.tucra@gmail.com>
2025-11-21 22:27:03 +0200

    fixed rotation problem

--- main.py
+++ main.py
@@ -20,10 +20,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        player.update(dt)
+
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
