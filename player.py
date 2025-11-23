6e2d364782e2d48a8b94bfb41a93d0278f03b6bc
darth-kratos <catalin.tucra@gmail.com>
2025-11-23 16:47:34 +0200

    finally made shots to work

--- player.py
+++ player.py
@@ -1,6 +1,14 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import (
+    PLAYER_RADIUS,
+    LINE_WIDTH,
+    PLAYER_TURN_SPEED,
+    PLAYER_SPEED,
+    PLAYER_SHOOT_SPEED,
+    SHOT_RADIUS,
+)
 import pygame
+from shot import Shot
 
 
 class Player(CircleShape):
@@ -40,3 +48,9 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_w]:
             self.move(dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
